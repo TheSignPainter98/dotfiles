50067675590297bb901d7dbceab339c002ddfc58
kcza <ed@kcza.net>
2026-03-03 16:56:00 +0000

    Add dotfiles

---
+++ home/.config/nvim/queries/go/injections.scm
@@ -0,0 +1,19 @@
+; Embedded starlark
+(call_expression
+	function: (selector_expression
+		operand: (identifier) @_id (#eq? @_id "st")
+		field: (field_identifier) @_field (#eq? @_field "RunString")
+	)
+	arguments: (argument_list
+		(raw_string_literal) @python ; (#offset! @python 1 0 -1 0)
+	)
+)
+(call_expression
+	function: (selector_expression
+		operand: (identifier) @_id (#eq? @_id "st")
+		field: (field_identifier) @_field (#eq? @_field "SetExecutionStepModel")
+	)
+	arguments: (argument_list
+		(raw_string_literal) @python ; (#offset! @python 1 0 -1 0)
+	)
+)
---
+++ home/.config/nvim/queries/rust/injections.scm
@@ -0,0 +1,26 @@
+; Embedded lua
+(call_expression
+  function: (field_expression
+    field: (field_identifier) @_field (#eq? @_field "load")
+  )
+  arguments: (arguments
+    (macro_invocation
+	  macro: (identifier) @_macro (#eq? @_macro "chunk")
+	  (token_tree) @lua (#offset! @lua 1 0 -1 0)
+    )
+  )
+)
+
+; Embedded sql
+(call_expression
+  function: (scoped_identifier
+	path: (identifier) @_pkg (#eq? @_pkg "sqlx")
+	name: (identifier) @_func (#eq? @_func "query"))
+  arguments: (arguments
+	(string_literal) @sql (#offset! @sql 1 0 -1 0)))
+(call_expression
+  function: (scoped_identifier
+	path: (identifier) @_pkg (#eq? @_pkg "sqlx")
+	name: (identifier) @_func (#eq? @_func "query"))
+  arguments: (arguments
+	(raw_string_literal) @sql (#offset! @sql 3 0 -2 0)))
