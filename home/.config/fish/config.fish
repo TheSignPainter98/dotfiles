1f8aa0239a1def5d8132e5b0b2ed397466677f04
kcza <ed@kcza.net>
2026-05-01 13:14:42 +0100

    Add `~/.npm-global` to path

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,6 +3,7 @@ set fish_greeting
 fish_add_path ~/go/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
+fish_add_path ~/.npm-global/bin
 fish_add_path /usr/lib/ccache
 fish_add_path /snap/rustup/current/bin
 
